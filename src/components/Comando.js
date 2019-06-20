9d60d1ff9b33cd59929a3da0e006ace52ab70728
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-06-20 13:44:25 -0300

    Botão calcular e o contexto léxico
    1 - Delegando variáveis de estado e funções via props para componentes filhos.
    2 - Botão calcular e o contexto léxico.

--- src/components/Comando.js
+++ src/components/Comando.js
@@ -3,5 +3,5 @@ import React from 'react';
 import { Button } from 'react-native';
 
 export default props => (
-    <Button title="Calcular" />
+    <Button title="Calcular" onPress={props.acao} />
 );
--- src/components/Numero.js
+++ src/components/Numero.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
 export default props => (
-    <TextInput style={styles.numero} />
+    <TextInput style={styles.numero} value={props.num} />
 );
 
 const styles = StyleSheet.create({
