e408bc05214247056c5ac8f80586dd14b5d59c77
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-06-20 14:46:04 -0300

    Componente Operacao - Refactoring
    Componente Operacao - Refactoring.

--- src/components/Painel.js
+++ src/components/Painel.js
@@ -10,15 +10,20 @@ class Painel extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { num1: '1', num2: '2' };
+        this.state = { num1: '1', num2: '2', operacao: 'subtracao' };
 
         this.calcular = this.calcular.bind(this);
         this.atualizaValor = this.atualizaValor.bind(this);
+        this.atualizaOperacao = this.atualizaOperacao.bind(this);
     }
 
     calcular() {
         const resultado = parseFloat(this.state.num1) + parseFloat(this.state.num2);
         console.log(resultado);
+    }
+
+    atualizaOperacao(operacao) {
+        this.setState({ operacao });
     }
 
     atualizaValor(nomeCampo, numero) {        
@@ -36,7 +41,7 @@ class Painel extends Component {
                     num2={this.state.num2} 
                     atualizaValor={this.atualizaValor}
                 />
-                <Operacao />
+                <Operacao operacao={this.state.operacao} atualizaOperacao={this.atualizaOperacao} />
                 <Comando acao={this.calcular} />
             </View>
         );
